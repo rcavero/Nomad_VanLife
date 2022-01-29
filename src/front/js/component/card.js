4a530c606ae342a625b55ffe83c7c306525aa571
rcavero <ramoncaveroaras@gmail.com>
2022-01-29 12:13:04 +0000

    Adden new features to cards view with props

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -2,30 +2,64 @@ import propTypes from "prop-types";
 import React from "react";
 
 export const Card = (props) => {
+
+    const kinfOfPlace = (i) => {
+        let place = ""
+        if (i==0){
+            place = "fas fa-campground text-success"
+        }else if (i==1){
+            place = "fas fa-caravan text-success"
+        }else if (i==2){
+            place = "fas fa-laptop-house text-success"
+        }else{
+            place = "fas fa-question text-success"
+        }
+        return place
+    }
+    
+    const showService = (n) => {
+        let show=""
+        if (props.services[n] === 0){
+            show="collapse"
+        }
+        return show
+    }
+
+    const rateStar = (pos,rate) => {
+        let light=""
+        let stars=[0,0,0,0,0]
+        if (pos<=rate-1){
+            light="fas fa-star text-warning"
+        }else{
+            light="fas fa-star text-secondary"
+        }
+        return light
+    }
+
     return (
         <div className="container-fluid mt-3">
-            <div className="card mb-3 col-lg-6 col-md-10 col-sm-12 m-auto">
+            <div className="card mb-3 col-lg-6 col-md-10 col-sm-12 m-auto bg-light shadow">
                 <img src={props.picture} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title mb-3"><i class="fas fa-campground text-success"></i> {props.title}</h5>
+                    <h4 className="card-title mb-3"><i class={kinfOfPlace(props.kindPlace)}></i> {props.title}</h4>
                     <p className="card-text"><i class="fas fa-map-marker-alt text-danger"></i> {props.adress}</p>
-                    <p className="card-text">Este es un lugar perfecto en medio del desierto en el que nadie te va a molestar y puedes pasar la noche tranquilamente. Espectaculares las vistas del amanecer mientras tomas un café.</p>
+                    <p className="card-text">{props.description}</p>
                     <div className="container-fluid d-flex justify-content-between ps-0 mb-3">
                         <div>
                             <h5 className="card-text text-primary">
-                                <i class="fas fa-shower me-2"></i>
-                                <i class="fas fa-laptop me-2"></i>
-                                <i class="fas fa-gas-pump me-2"></i>
-                                <i class="fas fa-bed"></i>
+                                <span className={showService(0)}><i class="fas fa-shower me-2"></i></span>
+                                <span className={showService(1)}><i class="fas fa-laptop me-2"></i></span>
+                                <span className={showService(2)}><i class="fas fa-gas-pump me-2"></i></span>
+                                <span className={showService(3)}><i class="fas fa-bed"></i></span>
                             </h5>
                         </div>
                         <div>
                             <h5 className="card-text">
-                            <i class="fas fa-star text-warning"></i>
-                            <i class="fas fa-star text-warning"></i>
-                            <i class="fas fa-star text-warning"></i>
-                            <i class="fas fa-star text-warning"></i>
-                            <i class="fas fa-star text-secondary"></i>
+                            <i class={rateStar(0, props.rate)}></i>
+                            <i class={rateStar(1, props.rate)}></i>
+                            <i class={rateStar(2, props.rate)}></i>
+                            <i class={rateStar(3, props.rate)}></i>
+                            <i class={rateStar(4, props.rate)}></i>
                             </h5>
                         </div>
                     </div>
@@ -38,5 +72,9 @@ export const Card = (props) => {
         title: propTypes.string,
         adress: propTypes.string,
         picture: propTypes.string,
+        description: propTypes.string,
+        services: propTypes.array,
+        rate: propTypes.number,
+        kindPlace: propTypes.number,
     }
 }
