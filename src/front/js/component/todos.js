38956375ea4384c518ca6f64f9ea0fd258e4b1c7
rcavero <ramoncaveroaras@gmail.com>
2022-02-08 00:33:25 +0000

    Completed wishlist but needed to fix the lag in delete todos

--- src/front/js/component/todos.js
+++ src/front/js/component/todos.js
@@ -13,22 +13,44 @@ export const Todos = (props) => {
 	};
 
 	return (
-		<ol className="row">
-			{props.listTodos.map((value, index) => {
-				return (
-					<li key={index}>
-						{value.label}
-						<button
-							type="button"
-							class="btn-close"
-							aria-label="Close"
-							onClick={() => {
-								deleteTodo(index);
-							}}></button>
-					</li>
-				);
-			})}
-		</ol>
+		<div className="container-fluid justify-content-center px-0 mt-3">
+			{/* <ol className="row">
+				{props.listTodos.map((value, index) => {
+					return (
+						<li key={index}>
+							{value.label}
+							<button
+								type="button"
+								class="btn-close"
+								aria-label="Close"
+								onClick={() => {
+									deleteTodo(index);
+								}}></button>
+						</li>
+					);
+				})}
+			</ol> */}
+			<div className="card">
+				<ul className="list-group list-group-flush">
+					{props.listTodos.map((value, index) => {
+						return (
+							<li className="list-group-item d-flex justify-content-between align-items-center mt-1 mb-1 fw-bold fs-6" key={index}>
+								{value.label}
+								<button
+									type="button"
+									class="btn-close"
+									aria-label="Close"
+									onClick={() => {
+										deleteTodo(index);
+									}}>
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</div>
+
 	);
 };
 
--- src/front/js/pages/wishList.js
+++ src/front/js/pages/wishList.js
@@ -9,7 +9,6 @@ const WhishList = () => {
 	const [todo, setTodo] = useState("");
 
 	useEffect(() => {
-        console.log("Hola Mundo")
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/RCavero")
 			.then((response) => response.json())
 			.then((response) => {
@@ -42,23 +41,29 @@ const WhishList = () => {
 	};
 
 	return (
-		<div className="text-center mt-5">
-			<h1>To Do List</h1>
-			<input
-				type="text"
-				name="todo"
-				onChange={(event) => {
-					return setTodo(event.target.value);
-				}}
-				value={todo}
-			/>
-			<button
-				onClick={() => {
-					addTodo();
-					setTodo("");
-				}}>
-				Add task
-			</button>
+		<div className="container-fluid text-center col-xl-6 col-lg-7 col-md-8 col-sm-10 col-12 mt-5 mb-5">
+			<h1 className="mb-5">Wish List</h1>
+			
+			<div className="input-group mb-4">
+				<input type="text" name="todo" className="form-control" placeholder="Add something to your Wish List" aria-label="Recipient's username" aria-describedby="button-addon2"
+					onChange={(event) => {
+						return setTodo(event.target.value);
+					}}
+					value={todo}
+					onKeyPress={(event) => {
+						if (event.key === 'Enter') {
+							addTodo();
+							setTodo("");
+						}
+					}}
+				/>
+					<button className="btn btn-success" type="button" id="button-addon2" 
+						onClick={()=>{
+							addTodo();
+							setTodo("");
+						}}
+					>Add Task</button>
+			</div>
 			<Todos listTodos={todos} functionDelete={deleteTodo} />
 		</div>
 	);
