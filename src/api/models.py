aa486682da565d6a32d82e4c4791640ade6b5d04
rcavero <ramoncaveroaras@gmail.com>
2022-02-08 21:40:55 +0000

    Models created and Backend engine working... Let's go!

--- src/api/models.py
+++ src/api/models.py
@@ -20,20 +20,41 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+class KindOfPlace(db.Model):
+    __tablename__ = 'kindofplace'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    icon = db.Column(db.String(80), unique=True, nullable=False)
+
+    nomadvanplace = db.relationship('NomadVanPlace', backref='kind_place', lazy=True)
+
+    def __repr__(self):
+        return '<Services %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "icon": self.icon,
+            # do not serialize the password, its a security breach
+        }
+
+
 class NomadVanPlace(db.Model):
+    __tablename__ = 'nomadvanplace'
     id = db.Column(db.Integer, primary_key=True)
     #user = db.Column(db.String(120), unique=True, nullable=False)
     #kind_of_place = db.Column(db.String(20), unique=True, nullable=False)
+    #location = db.Column(db.String(120), unique=False, nullable=False)
     services = db.Column(db.String(20), unique=True, nullable=False)
     title = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.String(500), unique=False, nullable=False)
-    #location = db.Column(db.String(120), unique=False, nullable=False)
     rating = db.Column(db.Float, unique=False, nullable=True)
     date = db.Column(db.Date, unique=False, nullable=False)
 
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     location = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
-    # kindofplace_id = db.Column(db.Integer, db.ForeignKey('kindofplace.id'), nullable=False)
+    kindofplace = db.Column(db.Integer, db.ForeignKey('kindofplace.id'), nullable=False)
     services = db.Column(db.Integer, db.ForeignKey('services.id'), nullable=False)
 
 
@@ -55,23 +76,7 @@ class NomadVanPlace(db.Model):
             # do not serialize the password, its a security breach
         }
 
-# class KindOfPlace(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(120), unique=True, nullable=False)
-#     icon = db.Column(db.String(80), unique=True, nullable=False)
 
-#     nomadvanplace = db.relationship('NomadVanPlace', backref='kind_places', lazy=True)
-
-#     def __repr__(self):
-#         return '<KindOfPlace %r>' % self.name
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "icon": self.icon,
-#             # do not serialize the password, its a security breach
-#         }
 
 class Services(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -96,7 +101,7 @@ class Locations(db.Model):
     lat = db.Column(db.Float, unique=True, nullable=False)
     lng = db.Column(db.Float, unique=True, nullable=False)
 
-    todos = db.relationship('Todos', backref='location_todo', lazy=True)
+    todos = db.relationship('Todos', backref='locations', lazy=True)
     nomadvanplace = db.relationship('NomadVanPlace', backref='location_place', lazy=True)
 
     def __repr__(self):
@@ -117,7 +122,7 @@ class Todos(db.Model):
     done = db.Column(db.Boolean, unique=False, nullable=False)
     #location = db.Column(db.String(120), unique=False, nullable=False)
 
-    location = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+    location = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
     user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
@@ -132,4 +137,3 @@ class Todos(db.Model):
             "location": self.location.id,
             # do not serialize the password, its a security breach
         }
-
